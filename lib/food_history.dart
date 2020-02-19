b09e20dadddf6f8502eb831cbba3f25f45bb3ca3
raymond-esparza <47377280+raymond-esparza@users.noreply.github.com>
2020-02-19 13:28:52 -0800

    Added cosmetic drop down box

--- lib/food_history.dart
+++ lib/food_history.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
 
 void openFoodHistoryPage(BuildContext context) {
+  final _historyPeriod  = ['Today', 'Yesterday', 'Past Week', 'Past Two Weeks', 'Past Month'];
+  String currentPeriodSelected = _historyPeriod[0];
+
   Navigator.push(context, MaterialPageRoute(
     builder: (BuildContext context) {
       return Scaffold(
         appBar: AppBar(
-          title: const Text('Calorie intake History'),
+          title: const Text('Calorie Intake History'),
         ),
-        body: const Center(
-          child: Text(
-            'This is the next page',
-            style: TextStyle(fontSize: 24),
-          ),
+        body: Column(
+          children: <Widget> [
+            Container(
+              child: Text('Intake History',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                  color: Colors.black
+                )
+              ),
+              padding: EdgeInsets.only(top: 10, left: 5)
+            ),
+            DropdownButton<String>(
+              items: _historyPeriod.map( (String menuItem){
+                return DropdownMenuItem<String> (
+                  value: menuItem,
+                  child: Text(menuItem)
+                );
+              } ).toList(),
+              onChanged: (String newPeriodSelected) {
+                //setState( () {
+                 // currentPeriodSelected = newPeriodSelected;
+               //} );
+              },
+              value: currentPeriodSelected
+            )
+          ]
         ),
       );
     },
-  ));
+  ) );
 }
