dab5d31d9b07cbf573aef6425797c5dc8f93e4e9
NathanJMarcos <NathanMarcos@CSU.Fullerton.edu>
2020-03-04 18:10:21 -0800

    Edit User Info UI updated
    Co-authored-by: Matt Watkins <mattwatkins@gmail.com>

--- lib/widgets/edit_user_info.dart
+++ lib/widgets/edit_user_info.dart
@@ -1,120 +1,173 @@
+import 'package:csuf_fitness/widgets/edit_user_info.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'user_info.dart';
 
-
 class EditUserInfo extends StatefulWidget {
-  
   EditUserInfo({Key key}) : super(key: key);
-
   @override
   _EditUserInfo createState() => _EditUserInfo();
 }
 
 class _EditUserInfo extends State<EditUserInfo> {
-  
-  final _formKey = GlobalKey<FormState>();
-
-  get firstname => null;
-
-  get lastname => null;
-
-  get age => null;
-
-  get height => null;
-
-  get weight => null;
-
+  static String firstname;
+  static String lastname;
+  static int age;
+  static int height;
+  static int weight;
+  final TextEditingController _inputFirstNameController =
+      TextEditingController();
+  final TextEditingController _inputLastNameController =
+      TextEditingController();
+  final TextEditingController _inputAgeController = TextEditingController();
+  final TextEditingController _inputHeightController = TextEditingController();
+  final TextEditingController _inputWeightController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Enter your first name',
-            ),
-            
-            controller: firstname,
-            validator: (firstname) {
-              if (firstname.isEmpty) {
-                return 'Please enter a valid name';
-              }
-              return null;
-            },
-            
-            ),
-            
-            TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Enter your last name',
-            ),
-            controller: lastname,
-            validator: (lastname) {
-              if (lastname.isEmpty) {
-                return 'Please enter a valid name';
-              }
-              return null;
-            },
-            ),
-            
-            TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Enter your age',
-            ),
-            controller: age,
-            validator: (age) {
-              if (age.isEmpty && age is int) {
-                return 'Please enter a valid age';
-              }
-              return null;
-            },
-            ),
-            
-            TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Enter your height in inches',
-            ),
-            controller: height,
-            validator: (height) {
-              if (height.isEmpty && height is int) {
-                return 'Please enter a valid height';
-              }
-              return null;
-            },
-            ),
-             
-             TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Enter your weight in pounds',
-            ),
-            controller: weight,
-            validator: (weight) {
-              if (weight.isEmpty && weight is int) {
-                return 'Please enter a valid weight';
-              }
-              return null;
-            },
-            ),
-          
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: FlatButton(
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                }
-                setUserInfo();
-              },
-              child: Text('Submit'),
-            ),
-          ),
-          ],
-        ),
-    );
+    return Column(children: <Widget>[
+      Container(
+          padding: EdgeInsets.all(8),
+          child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    flex: 2,
+                    child: TextFormField(
+                        controller: _inputFirstNameController,
+                        decoration: InputDecoration(
+                            labelText: "Enter your first name"))),
+                Expanded(
+                    child: FlatButton(
+                        child: Text("Set First Name"), onPressed: setFirstName))
+              ])),
+      Container(
+          padding: EdgeInsets.all(8),
+          child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    flex: 2,
+                    child: TextFormField(
+                        controller: _inputLastNameController,
+                        decoration: InputDecoration(
+                            labelText: "Enter your last name"))),
+                Expanded(
+                    child: FlatButton(
+                        child: Text("Set Last Name"), onPressed: setLastName))
+              ])),
+      Container(
+          padding: EdgeInsets.all(8),
+          child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    flex: 2,
+                    child: TextFormField(
+                      controller: _inputAgeController,
+                      decoration: InputDecoration(labelText: "Enter your age"),
+                      keyboardType: TextInputType.numberWithOptions(
+                          signed: false, decimal: false),
+                    )),
+                Expanded(
+                    child:
+                        FlatButton(child: Text("Set Age"), onPressed: setAge))
+              ])),
+      Container(
+          padding: EdgeInsets.all(8),
+          child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    flex: 2,
+                    child: TextFormField(
+                      controller: _inputHeightController,
+                      decoration:
+                          InputDecoration(labelText: "Enter your height"),
+                      keyboardType: TextInputType.numberWithOptions(
+                          signed: false, decimal: false),
+                    )),
+                Expanded(
+                    child: FlatButton(
+                        child: Text("Set Height"), onPressed: setHeight))
+              ])),
+      Container(
+          padding: EdgeInsets.all(8),
+          child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    flex: 2,
+                    child: TextFormField(
+                      controller: _inputWeightController,
+                      decoration:
+                          InputDecoration(labelText: "Enter your weight"),
+                      keyboardType: TextInputType.numberWithOptions(
+                          signed: false, decimal: false),
+                    )),
+                Expanded(
+                    child: FlatButton(
+                        child: Text("Set Weight"), onPressed: setWeight))
+              ]))
+    ]);
   }
-  void setUserInfo(){
-    UserInfo(firstname, lastname, age, height, weight);
+
+  void setFirstName() async {
+    firstname = (_inputFirstNameController.text);
+    setState(() {
+      _inputFirstNameController.clear();
+    });
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setString('userFirstName', firstname);
+  }
+
+  void setLastName() async {
+    lastname = (_inputLastNameController.text);
+    setState(() {
+      _inputLastNameController.clear();
+    });
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setString('userLastName', lastname);
+  }
+
+  void setAge() async {
+    age = int.parse(_inputAgeController.text);
+    setState(() {
+      _inputAgeController.clear();
+    });
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setInt('userAge', age);
+  }
+
+  void setHeight() async {
+    height = int.parse(_inputHeightController.text);
+    setState(() {
+      _inputHeightController.clear();
+    });
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setInt('userHeight', height);
+  }
+
+  void setWeight() async {
+    weight = int.parse(_inputWeightController.text);
+    setState(() {
+      _inputWeightController.clear();
+    });
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setInt('userWeight', weight);
   }
 }
