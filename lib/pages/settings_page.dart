b7e7e9fb0f20d14cdbeab1d1978539596909ecba
Matt Watkins <mattwatkins@gmail.com>
2020-03-07 15:31:02 -0800

    Update settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import '../widgets/calorie_goal_input_widget.dart';
+// import '../widgets/calorie_goal_input_widget.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../food_log.dart';
 
 class SettingsPage extends StatefulWidget {
-  SettingsPage({Key key, this.title}) : super(key: key);
-  final String title;
+  SettingsPage({Key key}) : super(key: key);
+
   @override
   _SettingPageState createState() => _SettingPageState();
 
@@ -20,21 +23,87 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingsPage> {
+  TextEditingController controller = TextEditingController();
+  int maxCalories = 0;
+
+  @override
+  void initState() {
+    updatePreferences().whenComplete(() {});
+    super.initState();
+  }
+
+  Future<void> updatePreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      if (!prefs.containsKey('maxCalories')) {
+        prefs.setInt('maxCalories', 2000);
+      }
+
+      maxCalories = prefs.getInt('maxCalories');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
       ),
-      body: _buildSettingPage(),
+      body: _buildBody(),
     );
   }
 
-  Widget _buildSettingPage() {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          InputDailyCalorieGoalWidget(),
+  Widget _buildBody() {
+    return SettingsList(
+      sections: [
+        SettingsSection(
+          title: 'Calorie Tracking',
+          tiles: [
+            SettingsTile(
+              title: 'Calorie Goal',
+              subtitle: maxCalories.toString(),
+              onTap: _showCalorieEntryDialog,
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  Future<void> _onCalorieEntryOkButtonPressed() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      maxCalories = int.parse(controller.text);
+      prefs.setInt('maxCalories', maxCalories);
+      FoodLog().maxCalories = maxCalories;
+    });
+
+    Navigator.pop(context);
+  }
+
+  void _showCalorieEntryDialog() async {
+    controller = TextEditingController();
+
+    await showDialog<String>(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          autofocus: true,
+          keyboardType:
+              TextInputType.numberWithOptions(signed: false, decimal: false),
+          controller: controller,
+          decoration: InputDecoration(hintText: "Amount"),
+        ),
+        actions: <Widget>[
+          FlatButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          FlatButton(
+              child: Text("OK"), onPressed: _onCalorieEntryOkButtonPressed),
         ],
       ),
     );
