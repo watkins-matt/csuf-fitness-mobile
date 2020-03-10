cb53be331f139c5326fc3c4c13730d79e24b4457
Matt Watkins <mattwatkins@gmail.com>
2020-03-09 21:36:36 -0700

    Add unit test for FDC API

---
+++ test/product_info_test.dart
@@ -0,0 +1,25 @@
+// import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:csuf_fitness/product_info.dart';
+import 'package:csuf_fitness/api_key.dart';
+
+void main() {
+  test('Is Product Title: Toaster Pastries?', () async {
+    APIKey.foodDataCentral = 'dp5nZdJdElWh76HXcIc6zxMNGomOE9K3UJp5drAv';
+    ProductInfoProvider pip = FoodDataCentralDataProvider();
+
+    String test = '038000304101';
+    String output = await pip.getProductTitle(test);
+    expect(output, "Unfrosted Toaster Pastries, Strawberry");
+  });
+
+  test('Toaster Pastry Calories: 200?', () async {
+    APIKey.foodDataCentral = 'dp5nZdJdElWh76HXcIc6zxMNGomOE9K3UJp5drAv';
+    ProductInfoProvider pip = FoodDataCentralDataProvider();
+
+    String test = '038000304101';
+    double output = await pip.getCalories(test);
+    int result = output.round();
+    expect(result, 200);
+  });
+}
