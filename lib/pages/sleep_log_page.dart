1636b8348103f926903e3c6754d5016bfeedaf60
Matt Watkins <mattwatkins@gmail.com>
2020-03-14 18:22:35 -0700

    Sleep tracking UI updates

--- lib/pages/sleep_log_page.dart
+++ lib/pages/sleep_log_page.dart
@@ -1,3 +1,4 @@
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
@@ -23,14 +24,61 @@ class _SleepLogPageState extends State<SleepLogPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.snooze),
           onPressed: () {},
         ),
+        body: _body(),
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: Text("My Health & Fitness"),
         ),
         drawer: MainDrawer());
+  }
+
+  Widget _header() {
+    return Container(
+        child: Card(
+            child: Row(children: <Widget>[
+      Text("Status: Awake"),
+      CircleAvatar(child: Text('0.0'))
+    ])));
+  }
+
+  Widget _body() {
+    return Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+          _header(),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: _buildListViewItem,
+            itemCount: 7,
+            padding: EdgeInsets.only(right: 8.0, bottom: 8.0, top: 8.0),
+          ))
+        ]));
+  }
+
+  Widget _buildListViewItem(BuildContext context, int index) {
+    List<String> titles = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+
+    Card listViewCard = Card(
+        child: ExpansionTileCard(
+            title: Text(titles[index]),
+            // subtitle: Text(DateFormat.jm().format(widget.log[index].time)),
+            trailing: CircleAvatar(child: Text('0.0'))));
+
+    //return Dismissible(key: UniqueKey(), child: listViewCard);
+    return listViewCard;
   }
 }
