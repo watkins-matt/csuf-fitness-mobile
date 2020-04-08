629b90e628382070748d4a826c03ff0a8c2c92b7
Anthony Leal <Tonyleal10@csu.fullerton.edu>
2020-04-08 16:32:08 -0700

    starting about_us

---
+++ lib/pages/about_us.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class about_us extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+      ),
+      );
+  }
+}
