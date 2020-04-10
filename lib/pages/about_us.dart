659feb753e389b948aa8c5f3380bf3f72f7f312c
Anthony Leal <Tonyleal10@csu.fullerton.edu>
2020-04-09 17:03:24 -0700

    update

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -9,8 +9,22 @@ class about_us extends StatelessWidget {
         brightness: Brightness.light,
         backgroundColor: Colors.blue,
         elevation: 0,
-        title: Text("Who We Are", style: TextStyle(color: Colors.white, fontSize: 20),),
+        title: Text("Meet The Team", style: TextStyle(color: Colors.white, fontSize: 20),),
       ),
+     body: SafeArea(
+       child: Padding(
+         padding: EdgeInsets.all(20.0),
+         
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: <Widget> [
+          SizedBox(height: 10,),
+          Text("Licenses, Agreements, and Attributes", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,color: Colors.grey),),
+
+           ],
+         ),
+       ),
+     ),
       );
   }
 }
