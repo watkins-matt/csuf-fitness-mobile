556e809bbf54c71191cf12e2fc3be3e316255662
Anthony Leal <Tonyleal10@csu.fullerton.edu>
2020-05-06 13:59:08 -0700

    updates

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 class about_us extends StatelessWidget {
+  
+  Tween<double> _scaleTween = Tween<double>(begin: 1, end:2 );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       
       backgroundColor: Colors.white,
       appBar: AppBar(
+        
         brightness: Brightness.light,
         backgroundColor: Colors.blue,
         elevation: 0,
         title: Text("About", style: TextStyle(color: Colors.white, fontSize: 20),),
       ),
+     
      body: SafeArea(
+       
        child: SingleChildScrollView(
        child: Padding(
          padding: EdgeInsets.all(20.0),
@@ -21,33 +26,48 @@ class about_us extends StatelessWidget {
            crossAxisAlignment: CrossAxisAlignment.start,
            children: <Widget> [
           SizedBox(height: 10,),
-          Text("Licenses, Agreements, and Attributes", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,color: Colors.black),),
+          
+          TweenAnimationBuilder(
+            tween: _scaleTween,
+            
+            duration: Duration (seconds: 2),
+            builder: (context, scale, child) {
+              return Transform.scale(scale: scale, child: child);
+            },
+                
+                child: Text('Licenses',textAlign: TextAlign.right ,style: TextStyle(fontSize: 8, fontWeight: FontWeight.bold,color: Colors.black, height:0),
+              )
+            
+   
+         ),
+
+          //Text("Licenses, Agreements, and Attributes", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,color: Colors.black),),
            SizedBox(height: 20,),
            Text("""name: csuf_fitness
 description: Allows users to track their health and fitness.
 
 dependencies:
-  flutter:
-    sdk: flutter
-  shared_preferences: ^0.5.6+1
-  sqflite: ^1.2.1
-  path_provider: ^1.6.0
-  intl: ^0.16.1
-  flutter_rounded_progress_bar: ^0.1.2
-  calendar_strip: ^1.0.6
-  recase: ^3.0.0
-  http: ^0.12.0+4
-  settings_ui: ^0.2.0
-  openfoodfacts: ^0.1.16
-  flutter_spinkit: ^4.1.2
-  qrscan: ^0.2.17
-  expansion_tile_card: ^1.0.2+2
-  provider: ^4.0.4
-  fit_kit: ^1.1.1
-  datetime_picker_formfield: ^1.0.0
+flutter:
+sdk: flutter
+shared_preferences: ^0.5.6+1
+sqflite: ^1.2.1
+path_provider: ^1.6.0
+intl: ^0.16.1
+flutter_rounded_progress_bar: ^0.1.2
+calendar_strip: ^1.0.6
+recase: ^3.0.0
+http: ^0.12.0+4
+settings_ui: ^0.2.0
+openfoodfacts: ^0.1.16
+flutter_spinkit: ^4.1.2
+qrscan: ^0.2.17
+expansion_tile_card: ^1.0.2+2
+provider: ^4.0.4
+fit_kit: ^1.1.1
+datetime_picker_formfield: ^1.0.0
   
 
-  sqflite:
+Sqflite:
 Copyright (c) 2017, Alexandre Roux Tekartik.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -67,6 +87,119 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+flutter:
+The “Flutter” name and logo are trademarks owned by Google. These Brand Guidelines describe the appropriate uses of the Flutter trademarks by members of the developer community who have obtained our consent to use the trademarks pursuant to the Flutter Terms of Service. These guidelines will ensure that the Flutter trademarks are used in a manner that promotes Google’s mission to provide a free and open source SDK for crafting high-quality native interfaces on iOS and Android in record time, and are not associated with objectionable material, as determined by Google.
+
+Use of the Flutter trademarks that is not expressly permitted by these guidelines is prohibited absent written permission from Google.
+
+The official Flutter logos can be found in the Flutter and Dart logo assets.
+
+
+Intl:
+
+Copyright 2013, the Dart project authors. All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of Google Inc. nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+path_provider:
+
+Copyright 2017, the Flutter project authors. All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of Google Inc. nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+Fit Kit:
+
+BSD 2-Clause License
+
+Copyright (c) 2019, Martin Anderson
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+Spinkit: 
+
+MIT License
+
+Copyright (c) 2018 Jeremiah Ogbomo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+
 
 """, 
                  
--- lib/pages/help_center.dart
+++ lib/pages/help_center.dart
@@ -7,24 +7,40 @@ class help_center extends StatelessWidget{
     return MaterialApp(
       title: 'Welcome To The Help Center ',
       home: Scaffold (
+        backgroundColor: Colors.white,
+      appBar: AppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        title: Text("About", style: TextStyle(color: Colors.white, fontSize: 20),),
+      ),
         body: Center (
+        
           child: TweenAnimationBuilder(
             tween: _scaleTween,
             duration: Duration (seconds: 2),
             builder: (context, scale, child) {
               return Transform.scale(scale: scale, child: child);
             },
-            
+                
                 child: Text(
-                'Welcome To The Help Center ',
+                'Welcome to our Licenses'
+                
+                ,
                 style: TextStyle(fontSize: 9, fontWeight: FontWeight.bold,color: Colors.black,height: 0),
               )
             
    
-         ),   
+         ),
+
+      
         
     ),
+    
     ),
+    
     );
+    
+    
 }
 }
