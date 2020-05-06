f68ccd26dc8a9ab758c3c6e3c07e986f388d656c
Matt Watkins <mattwatkins@gmail.com>
2020-05-06 13:22:16 -0700

    Implement HelpCenter push

--- lib/pages/help_center.dart
+++ lib/pages/help_center.dart
@@ -4,7 +4,14 @@ class HelpCenterPage extends StatefulWidget {
   @override
   _HelpCenterPageState createState() => _HelpCenterPageState();
 
-  static void push(BuildContext context) {}
+  static void push(BuildContext context) {
+    Navigator.pop(context);
+    Navigator.push(context, MaterialPageRoute(
+      builder: (BuildContext context) {
+        return Scaffold(body: HelpCenterPage());
+      },
+    ));
+  }
 }
 
 class _HelpCenterPageState extends State<HelpCenterPage> {
@@ -12,31 +19,30 @@ class _HelpCenterPageState extends State<HelpCenterPage> {
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome To The Help Center"),
-        ),
-
-        body: _buildBody(),
-      );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome To The Help Center"),
+      ),
+      body: _buildBody(),
+    );
   }
 
-      Widget _buildBody(){
-      return Center(
-          child: TweenAnimationBuilder(
-              tween: _scaleTween,
-              duration: Duration(seconds: 2),
-              builder: (context, scale, child) {
-                return Transform.scale(scale: scale, child: child);
-              },
-              child: Text(
-                'Welcome To The Help Center ',
-                style: TextStyle(
-                    fontSize: 9,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    height: 0),
-              )),
-        );
+  Widget _buildBody() {
+    return Center(
+      child: TweenAnimationBuilder(
+          tween: _scaleTween,
+          duration: Duration(seconds: 2),
+          builder: (context, scale, child) {
+            return Transform.scale(scale: scale, child: child);
+          },
+          child: Text(
+            'Welcome To The Help Center ',
+            style: TextStyle(
+                fontSize: 9,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                height: 0),
+          )),
+    );
   }
 }
